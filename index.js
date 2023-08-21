935eecca4e6466bab1c3bcad102ff1e12cff64fe
chyingh <57952828+chyingh@users.noreply.github.com>
2023-08-21 19:19:11 +0800

    1 file modified

--- index.js
+++ index.js
@@ -2,3 +2,25 @@ import './style.css';
 
 // 程式碼寫在這裡
 // 提示：BMI = 體重(kg) / 身高(m) 平方
+window.onload = function() {
+  var button = document.getElementsByTagName('button')[0];
+
+  button.addEventListener('click', function() {
+    var height = document.getElementById('bodyHeight').value;
+    var weight = document.getElementById('bodyWeight').value;
+    
+    if(height === '' || weight === '') {
+      alert('請輸入身高和體重');
+      return;
+    }
+  
+    // 身高從公分轉換為公尺
+    height = height / 100;
+    
+    // 計算 BMI
+    var bmi = weight / (height * height);
+    
+    // 將計算結果顯示在網頁上
+    document.getElementById('resultText').innerText = bmi.toFixed(2);
+  });
+}
